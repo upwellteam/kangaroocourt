6d6eab2a1a92efc7d95ffe779fe2f5cdeee299fe
Andrew Lukichev <andrewlukichev@gmail.com>
2015-04-17 21:30:48 +0300

    [fix] oauth credentials

--- front/src/scripts/app.js
+++ front/src/scripts/app.js
@@ -3,16 +3,16 @@ angular
         'ngRoute', 'ngCookies', 'ui.bootstrap', 'relativeDate', 'uiSwitch'
     ])
     .constant('OAUTH_PROVIDERS', {
-        // Local
-        facebook : {
-            client_id : '1430810303881868',
-            redirect_uri : 'http://kangaroo.loc:5000/oauth/facebook'
-        }
-        // Heroku
+        //// Local
         //facebook : {
-        //    client_id : '1620271311542333',
-        //    redirect_uri : 'http://kangaroocourt.herokuapp.com/oauth/facebook'
+        //    client_id : '1430810303881868',
+        //    redirect_uri : 'http://kangaroo.loc:5000/oauth/facebook'
         //}
+        //Heroku
+        facebook : {
+            client_id : '1620271311542333',
+            redirect_uri : 'http://kangaroocourt.herokuapp.com/oauth/facebook'
+        }
     })
     .constant('DISPUTE_CATEGORIES', [
         'Love',
