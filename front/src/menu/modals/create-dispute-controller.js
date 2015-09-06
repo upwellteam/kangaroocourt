d9b45b771ad3e962fcb71151565169c6bce46500
Andrew Lukichev <andrewlukichev@gmail.com>
2015-09-06 21:15:49 +0300

    [fix] cleanup new dispute var

--- front/src/menu/modals/create-dispute-controller.js
+++ front/src/menu/modals/create-dispute-controller.js
@@ -22,13 +22,11 @@ class CreateDisputeController {
 
         this.dispute = {
             isPrivate : false,
-            description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' +
-                'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in ' +
-                'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+            description : '',
             bet : 15,
             category : 'love',
             defendant : {
-                email : 'elizstiltzkin@gmail.com'
+                email : ''
             }
         };
     }
